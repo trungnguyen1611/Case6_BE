40401bdd04a4b3903883be53b858d03351e4864f
NQT <trungngxx@gmail.com>
2022-09-08 00:28:21 +0700

    deploy

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -8,8 +8,8 @@ const asyncWrapper = require("../Middleware/async");
 const mailer = require("../utils/mailer");
 const md5 = require('md5');
 const {sendMail} = require('../utils/mailer')
-const URL_FE = 'http://localhost:3000/'
-const URL_BE = 'http://localhost:8080/'
+const URL_FE = 'https://alohamoney.vercel.app/'
+const URL_BE = 'https://apialoha.vercel.app/'
 
 const defaultAvatar = "https://firebasestorage.googleapis.com/v0/b/aloha-money.appspot.com/o/DefaultUser.jpg?alt=media&token=58615f07-c33a-42f7-aa11-43b9d8170593"
 
